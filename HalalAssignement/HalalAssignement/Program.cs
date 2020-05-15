c571ebd55f73ecdb2b43f291eed0c6b7ed32480c
goet <goetsewer@gmail.com>
2020-05-15 15:58:05 +0200

    initial commit

---
+++ HalalAssignement/HalalAssignement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HalalAssignement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
